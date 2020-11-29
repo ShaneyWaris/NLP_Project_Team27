b0be794637b8c0391af08b3387f900b7d78156e6
ShaneyWaris <shaney18308@iiitd.ac.in>
2020-11-30 02:26:05 +0530

    Some updates!!

--- temp.py
+++
@@ -1,4 +0,0 @@
-l = [('RF LaBSE', 'discuss'), ('RF LaBSE+', 'unrelated'), ('RF UE', 'unrelated'), ('RF UE+', 'unrelated'), ('XGB LaBSE', 'agree'), ('XGB LaBSE+', 'agree'), ('XGB UE', 'unrelated'), ('XGB UE+', 'unrelated'), ('MLP LaBSE', 'agree'), ('MLP LaBSE+', 'agree'), ('MLP UE', 'agree'), ('MLP UE+', 'agree'), ('SVC LaBSE', 'agree'), ('SVC LaBSE+', 'unrelated'), ('SVC UE', 'agree'), ('SVC UE+', 'unrelated')]
-# print(l)
-row1 = l[0]
-print(row1[1])
